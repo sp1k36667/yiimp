de19b3d5628e6bd2daf021113c60b7c5067484e9
Mark Huetsch <markhuetsch@gmail.com>
2018-04-30 08:31:26 +0000

    add getpeerinfo support

--- web/yaamp/core/rpc/wallet-rpc.php
+++ web/yaamp/core/rpc/wallet-rpc.php
@@ -440,6 +440,18 @@ class WalletRPC {
 				$this->error = $this->rpc->error;
 				return $info;
 				break;
+			case 'getpeerinfo':
+				$info = $this->rpc->rpcget('/gateway');
+				$this->error = $this->rpc->error;
+				$btc_peer = function ($sia_peer)  {
+					return array(
+						"addr" => $sia_peer["netaddress"],
+						"version" => $sia_peer["version"],
+						"subver" => $sia_peer["version"],
+					);
+				};
+				return array_map($btc_peer, $info['peers']);
+				break;
 			}
 		}
 
