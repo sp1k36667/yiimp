630dbfa7c4cd6647c556b7e5e033a4244827d1dd
wangchao <wangchao719@gmail.com>
2018-06-18 09:45:52 +0800

    swich old algo to block_diff / pool_hash

--- web/yaamp/modules/site/results/mining_results.php
+++ web/yaamp/modules/site/results/mining_results.php
@@ -85,7 +85,9 @@ foreach($list as $coin)
 
 	$btcmhd = yaamp_profitability($coin);
 	$pool_hash = yaamp_coin_rate($coin->id);
-	$real_ttf = $pool_hash? $coin->difficulty * 0x100000000 / $pool_hash: 0;
+	// $real_ttf = $pool_hash? $coin->difficulty * 0x100000000 / $pool_hash: 0;
+	// swich old algo to block_diff / pool_hash
+	$real_ttf = $pool_hash? $coin->difficulty / $pool_hash: 0;
 
 	$pool_hash_sfx = $pool_hash? Itoa2($pool_hash).'h/s': '';
 	$real_ttf = $real_ttf? sectoa2($real_ttf): '';
